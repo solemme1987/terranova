7e7e435c72ed9740fdb8f7a2d307e1689b72d17e
Ricardo Cortes <ricacor3@gmail.com>
2021-02-22 15:01:06 -0500

    barra de busqueda completamente terminada

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,8 @@
 const  btnSearch =document.querySelector('#search')
 const searchInput = document.querySelector('.searchInput');
 
-
+// falta optimzar
 btnSearch.addEventListener('click', ()=>{
- 
    searchInput.classList.toggle('active')
    searchInput.select();
 })
